0f6f98fc1c421903fdafd25dd4aefa107abc1159
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-09-17 15:01:28 -0500

    conexion

---
+++ src/main/java/com/example/servidor_suratech_4/MODELOS/Paciente.java
@@ -0,0 +1,15 @@
+package com.example.servidor_suratech_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    private Integer id;
+    private String name;
+    private Integer edad;
+    private LocalDate fechaNacimiento;
+    private String documento;
+    private String telefono;
+    private Integer copago;
+    private String email;
+    private String ips;
+}
